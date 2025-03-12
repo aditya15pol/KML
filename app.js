d60dfc866dfb11d6dd5b81bceeea62c7379ed362
Aditya pol <adityapol4334@gmail.com>
2025-03-12 23:15:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Polyline } from "react-leaflet";
+import { parseString } from "xml2js";
+import "leaflet/dist/leaflet.css";
+import { divIcon } from "leaflet";
+
+const App = () => {
+  <div>Hi</div>
+};
+
+export default App;
